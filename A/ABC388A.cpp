85696bf7a63eeae8f97afa74e3c7646214e5248e
JEY HIRANO <mrjr0722@me.com>
2025-05-04 23:03:03 +0900

    毎日競プロ250504

---
+++ A/ABC388A.cpp
@@ -0,0 +1,16 @@
+/*
+g++ test.cpp
+./a.out
+
+Control + option + Nで実行
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << s.at(0) << "UPC" << endl;
+    return 0;
+}
