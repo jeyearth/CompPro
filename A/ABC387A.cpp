a940354104772a7384632d245f54a5d1b92497bf
JEY HIRANO <mrjr0722@me.com>
2025-05-11 23:11:42 +0900

    毎日競プロ250511

---
+++ A/ABC387A.cpp
@@ -0,0 +1,17 @@
+/*
+g++ test.cpp
+./a.out
+
+Control + option + Nで実行
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    cout << (A + B) * (A + B) << endl;
+    return 0;
+}
