cb42536bed040bf9fe789c2bf8393436c00195e9
JEY HIRANO <mrjr0722@me.com>
2025-05-14 16:03:45 +0900

    test.cppに変数チェックのためのPRINT_VARIABLE(var)を定義

--- test.cpp
+++ test.cpp
@@ -8,6 +8,8 @@ Control + option + Nで実行
 #include <bits/stdc++.h>
 using namespace std;
 
+// #define PRINT_VARIABLE(var) std::cout << #var << " : " << var << std::endl
+
 int main() {
     return 0;
 }
