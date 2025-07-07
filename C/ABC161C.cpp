8d392ddfbf7803416f5a1a5463a1fdb2615b8402
JEY HIRANO <mrjr0722@me.com>
2025-07-07 18:15:52 +0900

    毎日競プロ250707

---
+++ C/ABC161C.cpp
@@ -0,0 +1,21 @@
+/*
+g++ test.cpp
+./a.out
+
+Control + option + Nで実行
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define PRINT_VARIABLE(var) std::cout << #var << " : " << var << std::endl
+
+int main() {
+    long long n, k;
+    cin >> n >> k;
+
+    long long ans = n % k;
+    ans = min(ans, k - ans);
+    cout << ans << endl;
+    return 0;
+}
