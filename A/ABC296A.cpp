bc834c15997930b5a86981ee8b1faeebba2f3041
JEY HIRANO <mrjr0722@me.com>
2025-12-06 23:02:54 +0900

    毎日競プロ251206

---
+++ A/ABC296A.cpp
@@ -0,0 +1,27 @@
+/*
+g++ test.cpp
+./a.out
+
+Control + option + Nで実行
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define PRINT_VARIABLE(var) std::cout << #var << " : " << var << std::endl
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (s.at(i) == s.at(i + 1))
+        {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+    return 0;
+}
